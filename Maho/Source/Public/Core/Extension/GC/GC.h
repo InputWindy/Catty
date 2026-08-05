fd7d823bce5cb1740949f247558737bc91f2120a
InputWindy <365363471@qq.com>
2026-08-05 14:32:30 +0800

    清理引擎冗余空白文件及迁移遗留空目录

--- Maho/Source/Public/Core/Extension/GC/GC.h
+++
@@ -1,1 +0,0 @@
-﻿// Stub - GC moved to project
--- Maho/Source/Public/Core/Extension/Resource/Resource.h
+++
@@ -1,3 +0,0 @@
-// Redirect: Resource module moved to project.
-// Include the project-side authoritative header.
-#include "Game/System/Resource/ResourceSystem.h"
